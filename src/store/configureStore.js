1e89684afa08d562f28b7d46b67326a0e2479823
Bhavik Sheth <bhaviksheth20@gmail.com>
2018-03-16 20:09:06 +0000

    add module to throw error when state is mutated

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,11 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import ReduxImmutableStateInvariant from "redux-immutable-state-invariant"; // eslint-disable-line import/no-extraneous-dependencies
 import rootReducer from "../reducers/rootReducer";
 
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
+    applyMiddleware(ReduxImmutableStateInvariant()),
   );
 }
